3c29cb113562c584a4ee4ae71b36b9dffb08cb6d
Akshara Gopinathan <akshara.g@itp.objectfrontier.com>
2018-04-04 18:25:50 +0530

    Update exception

---
+++ src/main/java/com/ofs/Model/DuplicateNameException.java
@@ -0,0 +1,14 @@
+package com.ofs.Model;
+
+public class DuplicateNameException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public DuplicateNameException(String msg) {
+		super(msg);
+	}
+
+}
